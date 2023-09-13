f251d132936f52f64eb985004e8b425680c66633
joseRonald232001 <phjronald@gmail.com>
2023-09-13 16:02:48 -0400

    ...

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,15 +7,18 @@ const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const nav = [
-    { title: "eventos", to:"/" },
+    { title: "Eventos", to:"/" },
+    {title:"Tienda" ,to:"/"},
+    {title:"Registro" ,to:"/"},
+    {title:"Mi perfil" ,to:"/"}
   ];
 
   return (
     <>
-      <header className="absolute px-2 py-1 w-full">
-        <nav className="flex items-center ">
+      <header className=" relative max-w-[2020px]">
+        <nav className="flex items-center absolute px-2 py-1 lg:p-3 w-full  m-auto ">
           <Link to={"/"}>
-            <h1 className="font-blackOps text-white text-2xl">
+            <h1 className="font-blackOps text-white text-2xl md:text-3xl">
               AlaskaMu
             </h1>
           </Link>
@@ -23,11 +26,11 @@ const NavBar = () => {
           <FontAwesomeIcon icon={faBars} style={{color: "#fafafa",}} />
           </button>
 
-          <div className="text-white ml-auto hidden md:block">
+          <div className="text-white ml-auto hidden md:flex gap-5 lg:gap-16 lg:pr-10">
             {nav.map((page) => (
               <Link key={page.title} to={page.to}>
-                <ul>
-                  <li className="">{page.title}</li>
+                <ul className="text-lg">
+                  <li className="font-redrose">{page.title}</li>
                 </ul>
               </Link>
             ))}
--- src/pages/Hom.jsx
+++ src/pages/Hom.jsx
@@ -3,6 +3,7 @@ import mu from "../pictures/mu.mp4";
 import mufont from '../pictures/mufont.jpg'
 import picture2 from '../pictures/picture2.png'
 import picture3 from "../pictures/picture3.jpg";
+import alaskalogo from '../pictures/alaskalogo.png'
 import { Link } from "react-router-dom";
 
 const Hom = () => {
@@ -13,7 +14,7 @@ const Hom = () => {
 
   return (
     <>
-      <section className="w-full h-[80vh] ">
+      <section className="w-full h-screen md:h-fit m-auto max-w-[2020px]">
         <video
           className="w-full shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"
           autoPlay
@@ -24,7 +25,7 @@ const Hom = () => {
           <source src={mu} type="video/mp4" />
         </video>
 
-        <article className="w-full text-white relative  md:absolute md:top-24 ">
+        <article className="w-full text-white relative  md:absolute md:top-24 lg:top-52 ">
           <img
             className="absolute -z-10 w-full opacity-10 top-0 md:hidden"
             src={picture3}
@@ -34,8 +35,8 @@ const Hom = () => {
           <h2 className="text-emerald-400 font-redrose font-bold text-base md:hidden">
             AlaskaMu{" "}
           </h2>
-          <h3 className="font-bangers text-3xl md:text-4xl ">SERVIDOR PERSONALIZADO</h3>
-          <p className="text-sm md:text-lg">
+          <h3 className="font-bangers text-3xl md:text-4xl lg:text-6xl ">SERVIDOR PERSONALIZADO</h3>
+          <p className="text-sm md:text-lg lg:text-xl">
             ¡Libera tus habilidades en el campo de batalla!
           </p>
           <button className="bg-lime-500 p-2 rounded-md mt-10">
@@ -48,16 +49,27 @@ const Hom = () => {
         </article>
       </section>
 
-      <section className="w-full  text-white relative flex items-center">
+      <section className="w-full  text-white relative max-w-[2020px] m-auto ">
         <img 
         className="w-full absolute top-0 opacity-30 -z-10"
         src={mufont} alt="" />
-        <div className="w-1/2 z-10 ">
+        <article className="flex items-center px-2">
+
+        <div className="w-1/2 z-10  ">
             <img src={picture2} className="w-[80%]" alt="" />
         </div>
-        <div className="w-1/2 text-sm text-center">
-        <p>¡Forma parte de una comunidad de jugadores apasionados en el nuevo servidor de <span className="text-cyan-600">AlaskMu</span>!</p>
+        <div className="w-1/2  text-sm text-center  font-garamond md:text-lg lg:text-2xl lg:p-10">
+        <p>¡ Forma parte de una comunidad de jugadores apasionados en el  servidor de <span className="text-cyan-600">AlaskMu</span> !</p>
+        <img className="w-1/2 m-auto" src={alaskalogo} alt="" />
         </div>
+     
+        </article>
+      </section>
+
+      <section>
+
+        <br />
+        <br />
       </section>
     </>
   );
