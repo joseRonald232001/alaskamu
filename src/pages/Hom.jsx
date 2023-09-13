0c6da44d082710ed7b49d5f06f1c087729d0c4e6
joseRonald232001 <phjronald@gmail.com>
2023-09-13 14:29:30 -0400

    detalles

--- src/pages/Hom.jsx
+++ src/pages/Hom.jsx
@@ -20,18 +20,26 @@ const Hom = () => {
           <source src={mu} type="video/mp4" />
         </video>
 
-        <article className="w-full text-white text-center p-4">
-          <h2 className="text-emerald-400 font-redrose font-bold text-base">
-            AlaskaMu{" "}
-          </h2>
+        <article className="w-full text-white relative  ">
           <img
-            className="absolute -z-10 w-full opacity-10"
+            className="absolute -z-10 w-full opacity-10 top-0"
             src={picture3}
             alt="img"
           />
+          <div className="py-7 px-4 text-center">
+
+
+          <h2 className="text-emerald-400 font-redrose font-bold text-base">
+            AlaskaMu{" "}
+          </h2>
+
           <h3 className="font-bangers text-3xl ">SERVIDOR PERSONALIZADO</h3>
 
-          <p className="text-sm">¡Libera tus habilidades en el campo de batalla!</p>
+          <p className="text-sm">
+            ¡Libera tus habilidades en el campo de batalla!
+          </p>
+
+          </div>
         </article>
       </section>
     </>
